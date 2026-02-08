0b32180c3e97921dcf82389798fdb30948113e33
hourJsDev <hourjs168@gmail.com>
2026-02-08 13:34:12 +0700

    df

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
+import type { ChangeEvent } from "react";
 import { base64Encode } from "../utils/helper";
 import { CheckCircle, Copy } from "lucide-react";
 
